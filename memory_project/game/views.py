0fea70ebde6ba1fd9cb4083230eda528e2e53b08
Yeray Alonso Reyes <81472106+YerayAR@users.noreply.github.com>
2025-06-24 09:18:40 +0100

    Add Django Memory Game project with Docker setup

---
+++ memory_project/game/views.py
@@ -0,0 +1,46 @@
+import json
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .logic import GameBoard
+
+SESSION_KEY = 'game_state'
+
+
+def get_game(request):
+    data = request.session.get(SESSION_KEY)
+    game = GameBoard(data)
+    request.session[SESSION_KEY] = game.to_dict()
+    return game
+
+
+def index(request):
+    game = get_game(request)
+    context = {
+        'game': game,
+    }
+    return render(request, 'index.html', context)
+
+
+def flip_card(request, index):
+    game = get_game(request)
+    mismatch = False
+    if index == -1:
+        game.flip(None)
+    else:
+        mismatch = game.flip(index)
+    request.session[SESSION_KEY] = game.to_dict()
+    response = {
+        'cards': game.cards,
+        'states': game.states,
+        'moves': game.moves,
+        'win': game.is_win(),
+        'mismatch': mismatch,
+    }
+    return JsonResponse(response)
+
+
+def restart_game(request):
+    if SESSION_KEY in request.session:
+        del request.session[SESSION_KEY]
+    return redirect(reverse('index'))
